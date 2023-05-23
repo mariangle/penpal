4b7c84f4b4274e876f6e9bd0efc7c337d77268b3
Maria <mariale171001@gmail.com>
2023-05-24 00:30:07 +0200

    add: NextAuth config with GitHub provider

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,17 @@
+import NextAuth, { AuthOptions } from "next-auth"
+import GithubProvider from "next-auth/providers/github"
+
+
+export const authOptions: AuthOptions = {
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+  ],
+} 
+
+const handler = NextAuth(authOptions);
+
+
+export { handler as GET, handler as POST };
