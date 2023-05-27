8084f42c7e21dcaaaadf239e7cfe5a96b14bc9d7
Maria <mariale171001@gmail.com>
2023-05-27 21:28:47 +0200

    refactor: Navbar.tsx

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,26 +1,41 @@
 "use client"
 
-import { useSession, signIn, signOut } from "next-auth/react"
 import Button from "./Button"
+import Link from "next/link"
+import Icon from "./Icon"
+import UserCard from "./UserCard"
+import { HiUserCircle, HiOutlineMail, HiOutlineBell, HiOutlineMoon } from "react-icons/hi"
+
+import { useSession } from "next-auth/react"
 
 const Navbar = () => {
-    const { data: session, status } = useSession()
+    const { status } = useSession()
     
         return (
-          <div className="flex fixed w-screen top-0 justify-between bg-green-700 text-white p-2 items-center">
-            <div>SnailMail</div>
-            { (status === "unauthenticated" || status === "loading") && (
-              <div className="flex gap-2 items-center">
-                <Button onClick={() => signIn()} style="secondary">Sign in</Button>
-              </div>
-            )}
-            { status === "authenticated" && (
-            <div className="flex gap-2 items-center">
-              <img src={session?.user?.image || ""} alt="user-profile-image" width={20} height={20} className="rounded-full"/>
-              <p>Welcome back, {session?.user?.name}!</p>
-              <Button onClick={() => signOut()} style="primary">Sign out</Button>
+          <div className="flex fixed w-screen top-0 justify-between px-4 py-4 items-center border-b">
+            <div>
+              <Link href="/">SnailMail</Link>
             </div>
-            )}
+            <div className="flex gap-4 items-center">
+                {/* UNAUNATHENTICATED */}
+                { (status === "unauthenticated" || status === "loading") && (
+                  <>
+                    <Link href={"/login"} className="flex gap-2 items-center">
+                      <Button>Log In</Button>
+                    </Link>
+                  </>
+                )}
+                {/* AUTHENTICATED */}
+                { status === "authenticated" && (
+                  <>
+                    <Icon icon={HiOutlineMoon} size={20} color="grey"/>
+                    <Icon icon={HiOutlineMail} size={20} color="grey"/>
+                    <Icon icon={HiOutlineBell} size={20} color="grey"/>
+                    <UserCard showMenu={true} />
+                  </>
+                )}
+                {/* MOBILE NAV */}
+            </div>
           </div>
         )
     }
