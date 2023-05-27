203aabb813b71b6c6b8f4fd6e1f8efe6d90e383b
Maria <mariale171001@gmail.com>
2023-05-27 21:29:38 +0200

    create: UserCard.tsx with dropdown menu

---
+++ app/components/Icon.tsx
@@ -0,0 +1,15 @@
+import { IconType } from "react-icons/lib";
+
+interface IconProps {
+  icon: IconType;
+  size?: number;
+  color?: string;
+}
+
+const Icon: React.FC<IconProps> = ({ 
+    icon: IconComponent, size, color
+}) => {
+  return <IconComponent size={size} color={color} />;
+};
+
+export default Icon;
---
+++ app/components/UserCard.tsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import { HiChevronDown } from "react-icons/hi"
+
+import { useSession, signOut } from "next-auth/react"
+
+interface UserCardProps {
+  showMenu : boolean,
+}
+
+const UserCard: React.FC<UserCardProps> = ({
+  showMenu
+}) => {
+  const { data: session } = useSession()
+  const [showDropdown, toggleDropdown] = useState(false);
+
+  return (
+    <div className="relative">
+      <div className="flex items-center gap-2 cursor-pointer" onClick={() => toggleDropdown(!showDropdown)}>
+        <img src={session?.user?.image || ""} alt="Profile" className="w-8 h-8 rounded-full" />
+        <div>
+          <p className="text-sm font-medium">{session?.user?.name ?? ""}</p>
+          <p className="text-xs text-gray-500">{session?.user?.email || ""}</p>
+        </div>
+        <div className={`transform transition-transform ${showDropdown ? 'rotate-180' : ''}`}>
+          <HiChevronDown />
+        </div>
+      </div>
+      {showDropdown && showMenu && (
+        <div className="absolute top-full mt-2 bg-white border rounded-lg shadow-md w-full p-4 flex flex-col items-start">
+          <button>Edit Profile</button>
+          <button>Settings</button>
+          <button onClick={() => signOut()}>Sign Out</button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default UserCard;
