2f259b937a5cd61314ca6f0de26d344913ea007a
Maria <mariale171001@gmail.com>
2023-05-28 20:03:53 +0200

    create: route.ts for registering a user

---
+++ app/api/register/route.ts
@@ -0,0 +1,38 @@
+import bcrypt from "bcrypt"
+import prisma from "../../libs/prismadb"
+import { NextResponse } from "next/server"
+
+export async function POST(
+    request: Request
+){
+    const body = await request.json();
+    const { name, email, password, age, country } = body;
+
+    if (!name || !email || !password || !age || !country ){
+        return new NextResponse("Missing Fields", { status: 400})
+    }
+
+    const exists = await prisma.user.findUnique({
+        where: {
+            email
+        }
+    });
+
+    if (exists){
+        return new NextResponse("Email exists", { status: 400})
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await prisma.user.create({
+        data: {
+            email,
+            name,
+            hashedPassword,
+            age,
+            country,
+        }
+    })
+
+    return NextResponse.json(user);
+}
