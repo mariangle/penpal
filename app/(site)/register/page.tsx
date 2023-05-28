67ab9f2a02bb4ea49c975404e153c7bd46143d75
Maria <mariale171001@gmail.com>
2023-05-28 20:08:22 +0200

    add: register page

---
+++ app/(site)/register/page.tsx
@@ -0,0 +1,15 @@
+import AuthForm from "../../components/auth/AuthForm"
+
+const Register = () => {
+  return (
+    <div className='flex flex-col items-center justify-center h-screen'>
+      <h1>PenPal</h1>
+      <h2>Sign Up</h2>
+      <div className="mt-4 w-full max-w-sm">
+        <AuthForm variant="register"/>
+      </div>
+    </div>
+  )
+}
+
+export default Register
