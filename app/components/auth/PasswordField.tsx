d8aebe9d914f908c6db10a5e1350445394a7a9e4
Maria <mariale171001@gmail.com>
2023-05-28 20:04:57 +0200

    feat: implement feature to show password

---
+++ app/components/auth/PasswordField.tsx
@@ -0,0 +1,39 @@
+import { BsEye, BsEyeSlash } from "react-icons/bs";
+import { useState } from "react";
+import { UseFormRegister, FieldValues } from "react-hook-form"
+
+interface PasswordFieldProps {
+    id : string,
+    label : string,
+    required? : boolean,
+    register: UseFormRegister<FieldValues>,
+}
+
+const PasswordField : React.FC<PasswordFieldProps> = ({
+    id, label, register, required
+}) => {
+  const [showPassword, setShowPassword] = useState(false);
+  
+  return (
+    <div className="flex flex-col mb-4">
+      <label htmlFor="password" className="block text-sm font-medium text-gray-900">
+        {label}
+      </label>
+      <div className="relative">
+        <input
+          type={showPassword ? "text" : "password"}
+          {...register(id, { required })} 
+          className="w-full px-4 py-2 border border-gray-300 rounded focus:outline-none focus:ring focus:ring-blue-500"
+        />
+          <button
+            onClick={() => setShowPassword(!showPassword)}
+            className="absolute top-1/2 right-4 transform -translate-y-1/2 focus:outline-none"
+          >
+            {showPassword ? <BsEyeSlash size={20} /> : <BsEye size={20} />}
+          </button>
+      </div>
+    </div>
+  );
+};
+
+export default PasswordField;
