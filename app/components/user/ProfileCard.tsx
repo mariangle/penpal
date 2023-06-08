50e6b942253d031566572769409653635bae402d
Maria <mariale171001@gmail.com>
2023-06-08 02:01:33 +0200

    feat: display verified user with checkmark

--- app/components/user/ProfileCard.tsx
+++ app/components/user/ProfileCard.tsx
@@ -1,7 +1,9 @@
 import { IUser } from '@/app/types/User'
 import UserImage from './UserImage';
-import { GrLocation } from "react-icons/gr"
+import Icon from '../common/Icon';
+import { MdLocationOn } from "react-icons/md"
 import Link from 'next/link';
+import { TbSquareRoundedCheckFilled } from "react-icons/tb"
 
 interface ProfileCardProps {
   user: IUser;
@@ -14,11 +16,12 @@ const ProfileCard: React.FC<ProfileCardProps> = ({ user }) => {
         <UserImage user={user} size={60}/>
       </div>
       <div className='flex flex-col ml-4'>
-        <Link href={`/users/${user.id}`}>
-          <h3 className='font-bold mt-2'>{user.name}, {user.age}</h3>
+        <Link href={`/users/${user.id}`} className='flex gap-2 items-center'>
+          <h3 className='font-bold'>{user.name}, {user.age}</h3>
+          <div>{user.isVerified && (<Icon icon={TbSquareRoundedCheckFilled} color="#1174c5"/>)}</div>
         </Link>
         <div className='flex items-center gap-1'> 
-          <GrLocation />
+          <Icon icon={MdLocationOn} color="#183b7c"/>
           <div>{user.country}</div>
         </div>
         <div className='flex gap-1 mt-2 flex-wrap'>
