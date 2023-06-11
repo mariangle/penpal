51535a1fad4eb1ef074e5f416e393273c7cafa82
Maria <mariale171001@gmail.com>
2023-06-12 01:02:24 +0200

    feat: added panel to edit details directly from profile page

--- app/(site)/account/components/ProfileForm.tsx
+++ app/(site)/account/components/ProfileForm.tsx
@@ -1,57 +1,66 @@
 "use client"
 
-import Input from "@/app/components/Input"
-import Textarea from "@/app/components/Textarea"
-import Button from "@/app/components/Button"
+import Input from "@/app/components/Input";
+import Textarea from "@/app/components/Textarea";
+import Button from "@/app/components/Button";
 
-import { useFormatFullDate } from "@/app/util/useFormatDate"
+import { useFormatFullDate } from "@/app/util/useFormatDate";
 import { useForm, FieldValues, SubmitHandler } from "react-hook-form";
-import { IUser } from "@/app/types/User"
-import { useEffect, useState } from "react"
-import axios from "axios"
+import { IUser } from "@/app/types/User";
+import { useContext, useEffect, useState } from "react";
+import axios from "axios";
+import { toast } from "react-hot-toast";
+import { UserContext } from "@/app/context/UserContext";
 
-const ProfileForm = ({ user } : {user: IUser}) => {
-  const { register, handleSubmit, setValue } = useForm<FieldValues>()
-  const [ data, setData ] = useState<IUser | undefined>(undefined)
+const ProfileForm = () => {
+  const { user, setUser } = useContext(UserContext);
+  const { register, handleSubmit, setValue } = useForm<FieldValues>();
+  const [data, setData] = useState<IUser | undefined>(undefined);
 
   useEffect(() => {
-    setData(user)
+    setData(user);
     if (user) {
-      setValue("name", user.name)
-      setValue("image", user.image)
-      setValue("about", user.about)
-      setValue("country", user.country)
-      setValue("age", user.age)
+      setValue("name", user.name);
+      setValue("image", user.image);
+      setValue("about", user.about);
+      setValue("country", user.country);
+      setValue("age", user.age);
     }
-  }, [user, setValue])
+  }, [user, setValue]);
 
   const updateUser: SubmitHandler<FieldValues> = async (data) => {
-    console.log("userId", user.id);
-    console.log("data", data);
-  
-    const response = await axios.put(`/api/users/${user.id}`, {
-      ...data,
-      userId: user.id
+    try {
+      const response = await axios.put(`/api/users/${user?.id}`, {
+        ...data,
+        userId: user?.id,
       });
-  
-    console.log('response:', response.data);
+      setUser(response.data);
+      toast.success("Your profile has been updated.");
+    } catch (err) {
+      toast.error("An error occurred.");
+      console.log("Error updating user:", err);
+    }
   };
 
   return (
     <form onSubmit={handleSubmit(updateUser)}>
       <Input label="Name" id="name" type="text" register={register} />
-      <Input label="Profile Image" id="image" type="text" register={register} />
+      <Input label="Profile Image URL" id="image" type="text" register={register} />
       <Textarea label="Bio" id="about" rows={5} register={register} />
       <Input label="Country" id="country" type="text" register={register} />
       <Input label="Age" id="age" type="number" register={register} />
       <div className="flex gap-2 items-center justify-between">
         <div className="text-gray-600 text-sm">
-          Last Updated {data?.updatedAt && useFormatFullDate(new Date(data.updatedAt).toLocaleString())}
+          Last Updated{" "}
+          {data?.updatedAt &&
+            useFormatFullDate(new Date(data.updatedAt).toLocaleString())}
         </div>
-        <Button type="submit">Save</Button>
+        <div>
+          <Button type="submit">Save</Button>
+        </div>
       </div>
     </form>
-  )
-}
+  );
+};
 
-export default ProfileForm
+export default ProfileForm;
--- app/(site)/users/components/Overview.tsx
+++ app/(site)/users/components/Overview.tsx
@@ -1,11 +1,27 @@
 import Icon from '@/app/components/Icon';
 import UserImage from '@/app/components/ProfilePicture';
+import EditPanel from './EditPanel';
+import Button from '@/app/components/Button';
 
-import { TbSquareRoundedCheckFilled } from 'react-icons/tb';
-import { MdLocationOn } from 'react-icons/md';
+import { HiLocationMarker, HiCheckCircle } from 'react-icons/hi';
 import { IUser } from '@/app/types/User';
+import { useContext, useEffect, useState } from 'react';
+import { UserContext } from '@/app/context/UserContext';
 
-const ProfileOverview = ({ user }: { user: IUser }) => {
+const ProfileOverview = ({ profile }: { profile: IUser }) => {
+  const [isCurrentUser, setIsCurrentUser] = useState<boolean>(false);
+  const [showEditPanel, setShowEditPanel] = useState<boolean>(false);
+  const { user } = useContext(UserContext);
+
+  useEffect(() => {
+    if (user?.email === profile.email) {
+      setIsCurrentUser(true);
+    }
+  }, [user]);
+
+  const handleToggleEditPanel = () => {
+    setShowEditPanel(!showEditPanel);
+  };
 
   return (
     <div className="border p-1">
@@ -14,32 +30,40 @@ const ProfileOverview = ({ user }: { user: IUser }) => {
         CoverPhoto
       </div>
       {/* <---- LOWER SECTION ---->*/}
-      <div className='flex items-center flex-col md:flex w-full'>
+      <div className="flex items-center flex-col md:flex w-full">
         {/* PROFILE PICTURE AND ABOUT*/}
-        <div className='flex flex-col items-center w-full md:flex-row'>
-          <div className='-translate-y-8 w-44 h-44'>
-            <UserImage user={user} border />
+        <div className="flex flex-col items-center w-full md:flex-row">
+          <div className="-translate-y-8 w-44 h-44">
+            <UserImage user={profile} border />
           </div>
           <div className="md:flex justify-between items-start px-4 w-full">
             <div>
-              <div className='flex gap-2 items-center'>
+              <div className="flex gap-2 items-center">
                 <h1 className="font-bold text-xl blue_gradient">
-                  {user.name}, {user.age}
+                  {profile.name}, {profile.age}
                 </h1>
-                {user.isVerified && (
-                  <Icon icon={TbSquareRoundedCheckFilled} color="#1174c5" size={24} />
+                {profile.isVerified && (
+                  <Icon icon={HiCheckCircle} color="#1174c5" size={24} />
                 )}
               </div>
               <div className="flex gap-2 items-center bg-blue-100 text-blue-950 px-2 py-1 rounded-md mt-2">
-                <Icon icon={MdLocationOn} size={18} />
-                {user.country}
+                <Icon icon={HiLocationMarker} size={18} />
+                {profile.country}
               </div>
             </div>
             {/* PROFILE DETAILS*/}
-            <div className="flex flex-col justify-between md:flex-row md:items-center md:gap-4">
-              <button className="bg-black text-white p-2 rounded-md">Send Letter</button>
-            </div>
+            {!isCurrentUser && (
+              <div className="flex flex-col justify-between md:flex-row md:items-center md:gap-4">
+                <button className="outline_btn">Send Letter</button>
+              </div>
+            )}
+            {isCurrentUser && (
+              <Button onClick={handleToggleEditPanel}>
+                {showEditPanel ? 'Close Edit Panel' : 'Edit Profile'}
+              </Button>
+            )}
           </div>
+          {showEditPanel && user && <EditPanel user={user} onClick={() => setShowEditPanel(!showEditPanel)}/>}
         </div>
       </div>
     </div>
